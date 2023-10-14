1a4507c1d9111278a9e8cf164f016db37486b646
MaksimEiche33 <zederundbirke56@gmail.com>
2023-10-14 14:28:17 +0300

    1

---
+++ app/src/main/java/mai/team2/android_lr_2/MainActivity.java
@@ -0,0 +1,14 @@
+package mai.team2.android_lr_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
