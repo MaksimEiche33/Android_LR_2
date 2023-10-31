d4db49bef9c83a1184ab08966426ab87c505bb61
Losyash-89 <chetireit@yandex.ru>
2023-10-31 12:03:46 +0300

    making fragment_crime.xml

--- app/src/main/java/mai/team2/android_lr_2/Crime.java
+++ app/src/main/java/mai/team2/android_lr_2/Crime.java
@@ -4,13 +4,35 @@ import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
-    private UUID mId;
-    private String mTitle;
-    private Date mDate;
-    private boolean mSolved;
+    private UUID mId; // id преступления
+    private String mTitle; // название преступления
+    private Date mDate;  //
+    private boolean mSolved; // состояние преступления раскрыто/не раскрыто
 
     public Crime() {
-        mId = UUID.randomUUID();
-        mDate = new Date();
+        mId = UUID.randomUUID();   //генерация id
+        mDate = new Date();        // определение
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
